841016b5c6187e5beab549b3b8bd0d45c958ad3d
Makoorr <azizbenrejeb@gmail.com>
2024-11-13 22:59:32 +0100

    updated moving average

--- utils/movingaverage.py
+++ utils/movingaverage.py
@@ -1,9 +1,14 @@
-def moving_average(arr, window_size):
-    i = 0
-    moving_averages = []
-    while i < len(arr) - window_size + 1:
-        window = arr[i : i + window_size]
-        window_average = round(sum(window) / window_size, 2)
-        moving_averages.append(window_average)
-        i += 1
-    return moving_averages
+def moving_average(window, window_size):
+    window_average = round(sum(window) / window_size, 2)
+    
+    return window_average
+
+# def old_moving_average(arr, window_size):
+#     i = 0
+#     moving_averages = []
+#     while i < len(arr) - window_size + 1:
+#         window = arr[i : i + window_size]
+#         window_average = round(sum(window) / window_size, 2)
+#         moving_averages.append(window_average)
+#         i += 1
+#     return moving_averages
