59a1ccd22e41999696203aca994a52400aaf98c8
Aziz Ben Rejeb <99105286+Makoorr@users.noreply.github.com>
2024-12-25 19:08:49 +0100

    Delete docs directory

--- docs/run.ps1
+++
@@ -1,3 +0,0 @@
-# Stop-Process -Name mosquitto
-
-Start-Process -FilePath "C:\Program Files\mosquitto\mosquitto.exe" -ArgumentList '-v -c "C:\Program Files\mosquitto\mosquitto.conf"' -NoNewWindow -Wait
